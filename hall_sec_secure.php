c1ff8e9b023ffe4ad817c8a43d521cf3523f4b15
Rumbie <rumbidzaimatanga7@gmail.com>
2024-11-11 16:28:30 +0200

    Update hall_sec_secure.php

--- hall_sec_secure.php
+++ hall_sec_secure.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-if (!isset($_SESSION['access'])){
+if (!isset($_SESSION['access'])){ //if the user did.t enter their details from the login page they are redirected to the login page.
     header("Location: ../login_p/login.php");
     exit();
 }
